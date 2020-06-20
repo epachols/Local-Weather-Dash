65aa4335650b4d6ce256d6d5c2397738493dd3ad
Capitol Heel <66080081+epachols@users.noreply.github.com>
2020-06-20 15:50:35 -0700

    linking in FA icons, finishing base setup of input field

---
+++ script.js
@@ -0,0 +1,8 @@
+// left bar needs a search bar and button (static )
+// make search input 
+// make it locally saveable (ideas: button has data-name of city. and data-index of number in array of generated buttons
+// array of generated buttons pushed into when adding button, just like previous exercises.
+// need to link all 3 apis, test outcome. 
+// basic weather,
+// then 5 day forecast
+// then insert uv index into both
