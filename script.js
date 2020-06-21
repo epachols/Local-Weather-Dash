de620489cbfcbdf90832ace855926a456236de9f
Capitol Heel <66080081+epachols@users.noreply.github.com>
2020-06-21 02:11:38 -0700

    successful retrieval of first weather dash info. next up: finishing and styling card, then fiveday. over and out.

--- script.js
+++ script.js
@@ -20,16 +20,60 @@ function renderBtns() {
     b.attr("data-name", savedCities[i]);
     b.attr("data-index", [i]);
     b.text(savedCities[i]);
-    $("#cityBtns").prepend(a);
+    $("#cityBtns").prepend(b);
   }
+}
+
+// city, + date + overcast notation, temp, humidity, windspeed, uv index
+// NOTE:: since the following refers to THE BUTTON THAT"S CLICKED'S DATA NAME IT WON'T WORK.
+// RESET so that the thing passed into weatherGet (either data-name in case of button or this.prev().val().trim()
+function weatherGet() {
+  $("#today").empty();
+  var city = cityInput.val().trim();
+  var queryURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&APPID=844468539098ae6ed77db2290adaac81&units=imperial";
+  // Creating an AJAX call for the city being clicked
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    var daycard = $("<div card>");
+    // Storing the city name/date
+    var name =
+      response.name +
+      "(" +
+      moment().format("MMM Do YY") +
+      ")";
+    var pOne = $("<p>").addClass("h3").text(name);
+    // Displaying city/date
+    daycard.append(pOne);
+
+    // making and placing day weather icon 
+    var iconCode = response.weather[0].icon;
+    var icon = $("<img>").addClass("d-inline");
+    var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
+    icon.attr('src', iconUrl);
+    pOne.append(icon);
+
+    // Storing the temp
+    var temp = response.main.temp;
+    var pTwo = $("<p>").text("Temperature: " + temp + "°F");
+    // display temp
+    daycard.append(pTwo);
+
+    $("#today").prepend(daycard);
+  });
 }
 
 searchBtn.click(function () {
   event.preventDefault();
   var city = cityInput.val().trim();
-//  TODO: change this if statement to if there is a result 
+  //  TODO: change this if statement to if there is a result
   if (city) {
     savedCities.push(city);
+    weatherGet();
     cityInput.val("");
     // store savedCities to local storage with a for-each, store data-name as key and data-index as object
     renderBtns();
